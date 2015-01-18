1794e1160bdb2c31d9fc2108b94927cbb9e1954d
oggy83 <oggy8833@gmail.com>
2015-01-19 02:57:04 +0900

    first commit

---
+++ TinyOculusSharpDxDemo/LibOVR.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyOculusSharpDxDemo
+{
+	public static class LibOVR
+	{
+		[DllImport("libovr.dll")]
+		public extern static bool ovr_Initialize();
+
+		[DllImport("libovr.dll")]
+		public extern static void ovr_Shutdown();
+
+		[DllImport("libovr.dll")]
+		public extern static IntPtr ovrHmd_Create(int index);
+
+		[DllImport("libovr.dll")]
+		public extern static void ovrHmd_Destroy(IntPtr hmd);
+
+	}
+}
---
+++ TinyOculusSharpDxDemo/MyForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TinyOculusSharpDxDemo
+{
+	public partial class MyForm : Form
+	{
+		public MyForm()
+		{
+			InitializeComponent();
+		}
+
+		private void _OnLoad(object sender, EventArgs e)
+		{
+			if (!LibOVR.ovr_Initialize())
+			{
+				MessageBox.Show("Failed to initialize LibOVR.");
+				Close();
+				return;
+			}
+
+			m_hmd = LibOVR.ovrHmd_Create(0);
+			if (m_hmd == IntPtr.Zero)
+			{
+				MessageBox.Show("Oculus Rift not detected.");
+				Close();
+				return;
+			}
+		}
+
+		private void _OnFormClosed(object sender, FormClosedEventArgs e)
+		{
+			LibOVR.ovrHmd_Destroy(m_hmd);
+			LibOVR.ovr_Shutdown();
+		}
+
+		private IntPtr m_hmd = IntPtr.Zero;
+	}
+}
